e0d617980fb3d58cc270752e2765e31229d2fee3
MaxMillington <maxmillington89@gmail.com>
2015-08-17 17:32:22 -0600

    added user authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
             uniqueness: { case_sensitive: false},
             format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :password, presence: true, length: { minimum: 6 }
 
+  has_secure_password
 end
