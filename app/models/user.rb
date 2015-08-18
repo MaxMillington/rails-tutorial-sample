4294a27602384e469647e7b54eb9703ef87750cd
MaxMillington <maxmillington89@gmail.com>
2015-08-18 08:48:37 -0600

    added fixture data

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,14 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
             uniqueness: { case_sensitive: false},
             format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
 
-  has_secure_password
+
+  # Returns the hash digest of the given string.
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+        BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
